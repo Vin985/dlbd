f70e0ea83fc97842c8f2f9b988623c56e1165c31
Vin985 <sylvain.christin@gmail.com>
2020-09-16 12:13:02 -0400

    modified split_filder and list to allow for successive splits

--- src/dlbd/utils/split.py
+++ src/dlbd/utils/split.py
@@ -24,44 +24,61 @@ def random_split(split, paths):
     return {"training": training, "validation": validation}
 
 
-def split_files(files, proportion):
-    split1, split2 = [], []
-    n_files = len(files)
-    n_split = int(proportion * n_files)
-    split_idx = random.sample(range(0, n_files), n_split)
-    for i in range(0, n_files):
-        if i in split_idx:
-            split2.append(files[i])
-        else:
-            split1.append(files[i])
-    return (split1, split2)
+def split_list(data, split_props):
+    splits = []
+    tmp_list = data.copy()
+    random.shuffle(tmp_list)
+    for proportion in split_props:
+        split_length = int(len(tmp_list) * proportion)
+        splits.append(tmp_list[0:split_length])
+        tmp_list = tmp_list[split_length:]
+    splits.append(tmp_list)
+    return splits
 
 
-def split_folder(path, split, data_handler, database):
-    split1, split2 = [], []
+def split_folder(path, split_props):
+    splits = [[] for i in range(len(split_props) + 1)]
     dirs, files = list_folder(path, [".wav"])
     if files:
-        tmp_train, tmp_val = split_files(files, split)
-        split1 += tmp_train
-        split2 += tmp_val
+        tmp_splits = split_list(files, split_props)
+        for i, split in enumerate(tmp_splits):
+            splits[i] += split
     if dirs:
         for dir_path in dirs:
-            tmp_train, tmp_val = split_folder(dir_path, split, data_handler, database)
-            split1 += tmp_train
-            split2 += tmp_val
-    return (split1, split2)
+            tmp_splits = split_folder(dir_path, split_props)
+            for i, split in enumerate(tmp_splits):
+                splits[i] += split
+    return splits
 
 
-def arctic_split(paths, split, data_handler=None, database=None):
-    audio_path = paths["training_audio_dir"]
+def arctic_split(paths, data_handler=None, database=None):
+    audio_path = paths["audio"]["training"]
     if not audio_path.exists():
         raise ValueError(
-            "'audio_dir' option must be provided to split into training and validation subsets"
+            "'audio_dir' option must be provided to split into test, training and validation subsets"
         )
-    training, validation = split_folder(audio_path, split, data_handler, database)
+    split = data_handler.get_option("split", database, None)
+    if not split:
+        raise ValueError("Split option must be provided for arctic split function")
+    split_props = []
+    # * Make test split optional
+    test_split = split.get("test", 0)
+    if test_split:
+        split_props.append(test_split)
+    val_split = split.get("validation", 0.2)
+    split_props.append(val_split)
+    splits = split_folder(audio_path, split_props)
+    res = {}
+    idx = 0
+    if test_split:
+        res["test"] = splits[idx]
+        idx += 1
+    res["validation"] = splits[idx]
+    res["training"] = splits[idx + 1]
 
-    print(training, validation)
-    print(len(training), len(validation))
+    print([(k + " " + str(len(v))) for k, v in res.items()])
+    return res
+
     # Save file_list
     # data_handler.save_file_list("training", training, paths)
     # data_handler.save_file_list("validation", validation, paths)
