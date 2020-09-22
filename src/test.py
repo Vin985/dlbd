bfaff38c6ea3d7f7ed335e96ebc4585f8762f132
Vin985 <sylvain.christin@gmail.com>
2020-09-22 15:23:38 -0400

    training

--- src/test.py
+++ src/test.py
@@ -2,19 +2,25 @@ import yaml
 
 
 from dlbd.data.data_handler import DataHandler
-from dlbd.utils.split import arctic_split, split_list
+from dlbd.training.trainer import Trainer
+from dlbd.utils.split import arctic_split
+from dlbd.models.CityNetTF2 import CityNetTF2
 
 stream = open("CONFIG.yaml", "r")
 opts = yaml.load(stream, Loader=yaml.Loader)
 
-dh = DataHandler(opts)
+dh = DataHandler(opts, split_funcs={"arctic": arctic_split})
 
 
-# dh.create_datasets()
-dh.check_datasets(split_funcs={"arctic": arctic_split})
+# # dh.create_datasets()
+# dh.check_datasets(split_funcs={"arctic": arctic_split})
 
-train = dh.load_data("training")
-validate = dh.load_data("validation")
+# train = dh.load_data("training")
+# validate = dh.load_data("validation")
+
+model = CityNetTF2(opts)
+trainer = Trainer(opts, dh, model)
+trainer.train_model()
 
 # for database in dh.opts["data"]["databases"]:
 # print(dh.get_database_paths2(database, "train"))
