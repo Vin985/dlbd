08bac6aeb7c161fbcd89f2e9dbbe05e033017597
Vin985 <sylvain.christin@gmail.com>
2020-10-07 21:43:08 -0300

    evaluator refactor

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,5 +1,4 @@
 import os
-import yaml
 
 
 from dlbd.evaluation.evaluator import Evaluator
@@ -7,15 +6,15 @@ from dlbd.evaluation.evaluator import Evaluator
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-stream = open("src/evaluation_config.yaml", "r")
-opts = yaml.load(stream, Loader=yaml.Loader)
+# stream = open("src/evaluation_config.yaml", "r")
+# opts = yaml.load(stream, Loader=yaml.Loader)
 
-print(opts)
+# print(opts)
 
-for model in opts["models"]:
-    versions = model.get("versions", 1)
-    print(type(versions))
+# for model in opts["models"]:
+#     versions = model.get("versions", 1)
+#     print(type(versions))
 
-evaluator = Evaluator(opts)
+evaluator = Evaluator(opts_path="src/evaluation_config.yaml")
 
 evaluator.evaluate()
