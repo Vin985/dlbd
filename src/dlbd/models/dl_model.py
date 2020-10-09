eefb46f6b7c714867651a507b95870571707efae
Vin985 <sylvain.christin@gmail.com>
2020-10-09 16:18:56 -0300

    moved classify spectrogram to parent class better version initialisation check

--- src/dlbd/models/dl_model.py
+++ src/dlbd/models/dl_model.py
@@ -5,6 +5,7 @@ import librosa
 import numpy as np
 import yaml
 from librosa.feature import melspectrogram
+from tqdm import tqdm
 
 from ..data import utils
 from ..training.spectrogram_sampler import SpectrogramSampler
@@ -34,7 +35,7 @@ class DLModel:
     @property
     def model_name(self):
         if not self._model_name:
-            if self.version:
+            if self.version is not None:
                 self._model_name = self.NAME + "_v" + str(self.version)
             else:
                 return self.NAME
@@ -42,7 +43,7 @@ class DLModel:
 
     @property
     def version(self):
-        if not self._version:
+        if self._version is None:
             self._version = self.get_model_version(self.results_dir_root)
         return self._version
 
@@ -84,9 +85,16 @@ class DLModel:
         )
 
     def classify_spectrogram(self, spectrogram):
-        raise NotImplementedError(
-            "classify_spectrogram function not implemented for this class"
-        )
+        """Apply the classifier"""
+        tic = time()
+        test_sampler = SpectrogramSampler(self.opts)
+        labels = np.zeros(spectrogram.shape[1])
+        preds = []
+        for data, _ in tqdm(test_sampler([spectrogram], [labels])):
+            pred = self.predict(data)
+            preds.append(pred)
+        print("Classified {0} in {1}".format("spectrogram", time() - tic))
+        return np.vstack(preds)[:, 1]
 
     def prepare_data(self, data):
         return data
@@ -120,29 +128,6 @@ class DLModel:
         spec = np.log(self.opts["A"] + self.opts["B"] * spec)
         spec = spec - np.median(spec, axis=1, keepdims=True)
         return spec.astype(np.float32)
-
-    def classify(self, wavpath=None):
-        """Apply the classifier"""
-        tic = time()
-
-        if wavpath is None:
-            raise AttributeError("No wave path is provided")
-
-        wav, sr = self.load_wav(wavpath, loadmethod="librosa")
-        spec = self.compute_spec(wav, sr)
-
-        labels = np.zeros(spec.shape[1])
-        # print("Took %0.3fs to load" % (time() - tic))
-        tic = time()
-        probas = []
-        spec_sampler = SpectrogramSampler(self.opts)
-        for x, _ in spec_sampler([spec], [labels]):
-            pred = self.predict(x)
-            probas.append(pred)
-        # print("Took %0.3fs to classify" % (time() - tic))
-        print("Classified {0} in {1}".format(wavpath, time() - tic))
-
-        return {"preds": np.vstack(probas)[:, 1], "sr": sr}
 
     def save_params(self):
         utils.force_make_dir(self.results_dir)
