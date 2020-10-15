2d92de68ee765977537c04bf3613eac2d72fc1de
Vin985 <sylvain.christin@gmail.com>
2020-10-15 17:03:27 -0300

    citynet detector

--- src/dlbd/detectors/__init__.py
+++ src/dlbd/detectors/__init__.py
@@ -1,8 +1,11 @@
 from .standard_detector import StandardDetector
 from .subsampling_detector import SubsamplingDetector
 
+from .citynet_detector import CityNetDetector
+
 DETECTORS = {
     "standard": StandardDetector(),
     "subsampling": SubsamplingDetector(),
+    "citynet": CityNetDetector(),
 }
 
