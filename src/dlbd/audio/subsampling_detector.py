eb4c77e976eae72e36e5407c3885fab98dde41bd
Vin985 <sylvain.christin@gmail.com>
2020-12-01 22:44:13 -0400

    plot recordings changes in stats display

--- src/dlbd/audio/subsampling_detector.py
+++ src/dlbd/audio/subsampling_detector.py
@@ -5,8 +5,23 @@ from functools import partial
 
 import numpy as np
 import pandas as pd
+import plotnine
 
 from ..lib.detector import Detector
+
+from plotnine import (
+    aes,
+    element_text,
+    facet_wrap,
+    geom_bar,
+    geom_text,
+    ggplot,
+    geom_line,
+    theme,
+    theme_classic,
+    xlab,
+    ylab,
+)
 
 
 class SubsamplingDetector(Detector):
@@ -16,12 +31,12 @@ class SubsamplingDetector(Detector):
 
     def resample_max(self, x, threshold=0.98):
         if max(x) >= threshold:
-            return 2
+            return 1
         return 0
 
     def has_tag(self, x):
         if max(x) > 0:
-            return 1
+            return 2
         return 0
 
     def get_tag_index(self, x, step, tags):
@@ -90,7 +105,7 @@ class SubsamplingDetector(Detector):
         options = options or {}
         tags = tags[tags.recording_id == predictions.name]
         threshold = options.get("event_threshold", self.DEFAULT_EVENT_THRESHOLD)
-        predictions.loc[predictions.activity > threshold, "event"] = 2
+        predictions.loc[predictions.activity > threshold, "event"] = 1
         if not predictions.empty:
             step = predictions.time.values[1] - predictions.time.values[0]
             for tag in tags.itertuples():
@@ -98,26 +113,70 @@ class SubsamplingDetector(Detector):
                 end = math.ceil(tag.tag_end / step)
                 f = partial(self.list_append, y=tag.id)
                 predictions.tag_index[start:end].map(f)
+
         return predictions
 
     def match_events_apply(self, predictions, tags, options):
         recording_id = predictions.name
         current_tags = tags.loc[tags.recording_id == recording_id]
-        min_activity = options.get("min_activity", self.DEFAULT_MIN_ACTIVITY)
+        threshold = options.get("event_threshold", self.DEFAULT_EVENT_THRESHOLD)
         step = options.get("sample_step", self.DEFAULT_MIN_DURATION) * 1000
         resampler = predictions.resample(str(step) + "ms") if step else predictions
-        resample_func = functools.partial(self.resample_max, threshold=min_activity)
+        resample_func = functools.partial(self.resample_max, threshold=threshold)
         tmp_tags = {
             "id": current_tags.id.to_numpy().astype(str),
             "start": current_tags.tag_start.to_numpy(),
             "end": current_tags.tag_end.to_numpy(),
         }
         tag_func = functools.partial(self.get_tag_index, step=step, tags=tmp_tags)
-        res = resampler.agg({"activity": resample_func, "tag_index": tag_func}).rename(
-            columns={"activity": "event"}
+        res = (
+            resampler.agg({"activity": resample_func, "tag_index": tag_func})
+            .reset_index()
+            .rename(columns={"activity": "event", "datetime": "time"})
         )
         # res["recording_id"] = recording_id
         return res
+
+    def recording_plot(self, matches, options, tags_presence, step):
+
+        # y_true = y_true > 0.5
+
+        plot = (
+            ggplot(
+                data=matches,
+                mapping=aes(x="time", y="tag",),  # "factor(species, ordered=False)",
+            )
+            + geom_line(mapping=aes(y="event"), color="pink")
+            + geom_line(color="blue")
+            + geom_line(
+                mapping=aes(
+                    y=options.get("event_threshold", self.DEFAULT_EVENT_THRESHOLD)
+                ),
+                color="red",
+            )
+        )
+        if not step:
+            hww = 10
+            tags = matches.tag.values
+            y_true = np.zeros(len(tags))
+            for idx, _ in enumerate(tags):
+                start = max(idx - hww, 0)
+                end = min(idx + hww, tags.shape[0])
+                r = 1 if tags[start:end].max() > 0.5 else 0
+                y_true[idx] = r
+            plot = (
+                plot
+                + geom_line(mapping=aes(y=y_true), color="purple")
+                + geom_line(mapping=aes(y="activity"), color="orange")
+            )
+
+        return plot
+
+    def get_recording_plots(self, matches, options, tags_presence, step):
+        plots = matches.groupby("recording_id").apply(
+            self.recording_plot, options, tags_presence, step
+        )
+        plotnine.save_as_pdf_pages(plots, "test_save_plots_citynet.pdf")
 
     def get_stats(self, df, tags):
         # TODO: make stats coherent between no resampling and resampling: use tag index in the same way
@@ -125,39 +184,53 @@ class SubsamplingDetector(Detector):
 
         n_true_positives = len(res[res == 3])
         n_true_negatives = len(res[res == 0])
-        n_false_positives = len(res[res == 2])
-        n_false_negatives = len(res[res == 1])
+        n_false_positives = len(res[res == 1])
+        n_false_negatives = len(res[res == 2])
+
+        tagged_samples = len(df.tag[df.tag == 2])
 
         matched_tags = set(
             df.tag_index.loc[(df.tag_index.str.len() > 0) & (df.event > 0)].sum()
         )
 
+        n_matched_tags = len(matched_tags)
+
         n_tags = tags.shape[0]
-        n_unmatched_tags = n_tags - len(matched_tags)
+        n_unmatched_tags = n_tags - n_matched_tags
 
         precision = round(n_true_positives / (n_true_positives + n_false_positives), 3)
-        recall = round(n_true_positives / (n_true_positives + n_false_negatives), 3)
-        recall2 = round(n_true_positives / (n_true_positives + n_unmatched_tags), 3)
-        f1_score = round(2 * precision * recall / (precision + recall), 3)
+        recall_samples = round(
+            n_true_positives / (n_true_positives + n_false_negatives), 3
+        )
+        recall_tags = round(n_matched_tags / (n_matched_tags + n_unmatched_tags), 3)
+        f1_score = round(
+            2 * precision * recall_samples / (precision + recall_samples), 3
+        )
         return {
-            "n_events": int(df.event.sum() / 2),
+            "n_events": int(df.event.sum()),
             "n_tags": n_tags,
+            "n_tagged_samples": tagged_samples,
             "n_true_positives": n_true_positives,
             "n_false_positives": n_false_positives,
             "n_true_negatives": n_true_negatives,
             "n_false_negatives": n_false_negatives,
-            "n_tags_matched": len(matched_tags),
-            "n_tags_unmatched": n_unmatched_tags,
+            "n_matched_tags": n_matched_tags,
+            "n_unmatched_tags": n_unmatched_tags,
             "precision": precision,
-            "recall": recall,
-            "recall2": recall2,
+            "recall_sample": recall_samples,
+            "recall_tags": recall_tags,
             "f1_score": f1_score,
         }
 
     def evaluate(self, predictions, tags, options):
+        tags_df = tags["tags_df"]
+        tags_presence = {}
+        for idx, i in enumerate(tags["tags_linear_presence"]):
+            fp = str(tags["infos"][idx]["file_path"])
+            tags_presence[fp] = i
+
         preds = predictions.copy()
         # preds.loc[:, "tag_index"] = ""
-        tags = tags["tags_df"]
         preds.loc[:, "tag_index"] = pd.Series([[] for i in range(preds.shape[0])])
         preds.loc[:, "event"] = 0
         preds.recording_id = preds.recording_id.astype("category")
@@ -171,13 +244,14 @@ class SubsamplingDetector(Detector):
             apply_func = self.get_recording_events
 
         events = (
-            preds.groupby("recording_id", as_index=False, observed=True)
-            .apply(apply_func, tags=tags, options=options)
+            preds.groupby("recording_id", observed=True)
+            .apply(apply_func, tags=tags_df, options=options)
             .reset_index()
         )
         events.loc[:, "tag"] = 0
-        events.loc[events.tag_index.str.len() > 0, "tag"] = 1
+        events.loc[events.tag_index.str.len() > 0, "tag"] = 2
 
-        stats = self.get_stats(events, tags)
+        stats = self.get_stats(events, tags_df)
         print("Stats for options {0}: {1}".format(options, stats))
+        # self.get_recording_plots(events, options, tags_presence, step)
         return {"options": options, "stats": stats, "matches": events}
