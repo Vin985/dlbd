fe9253f40de99080d00464fcf529aea3e934b25e
Vin985 <sylvain.christin@gmail.com>
2020-12-09 00:32:52 -0400

    expand option utility

--- src/dlbd/utils/common.py
+++ src/dlbd/utils/common.py
@@ -1,4 +1,5 @@
 import collections.abc
+from itertools import product
 
 
 def deep_dict_update(original, update, copy=False):
@@ -18,3 +19,39 @@ def deep_dict_update(original, update, copy=False):
         else:
             original[key] = value
     return original
+
+
+def to_range(opts):
+    return list(range(opts["start"], opts["end"], opts.get("step", 1)))
+
+
+def expand_options_dict(options):
+    """Function to expand the options found in an options dict. If an option is a dict,
+    two possibilities arise:
+        - if the key "start" and "end" are present, then the dict is treated as a range and is
+        replaced by a list with all values from the range. A "step" key can be found to define
+        the step of the range. Default step: 1
+        - Otherwise, the dict is expanded in a recursive manner.
+
+    Args:
+        options (dict): dict containing the options
+
+    Returns:
+        list: A list of dicts each containing a set of options
+    """
+    res = []
+    tmp = []
+    for val in options.values():
+        if isinstance(val, dict):
+            if "start" in val and "end" in val:
+                tmp.append(to_range(val))
+            else:
+                tmp.append(expand_options_dict(val))
+        else:
+            if not isinstance(val, list):
+                val = [val]
+            tmp.append(val)
+    for v in product(*tmp):
+        d = dict(zip(options.keys(), v))
+        res.append(d)
+    return res
