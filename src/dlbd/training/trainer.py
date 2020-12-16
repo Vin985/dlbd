88e377e6e7317165ac8a5af5b73d9f29def95134
Vin985 <sylvain.christin@gmail.com>
2020-12-15 23:49:12 -0400

    API updates

--- src/dlbd/training/trainer.py
+++ src/dlbd/training/trainer.py
@@ -3,6 +3,10 @@ from ..utils.model_handler import ModelHandler
 
 class Trainer(ModelHandler):
     def train_model(self):
+        db_types = [
+            self.data_handler.DB_TYPE_TRAINING,
+            self.data_handler.DB_TYPE_VALIDATION,
+        ]
         if not self.data_handler:
             raise AttributeError(
                 "An instance of class DataHandler must be provided in data_handler"
@@ -10,10 +14,10 @@ class Trainer(ModelHandler):
             )
         if not self.model:
             raise AttributeError("No model found")
-        self.data_handler.check_datasets()
-        training_data = self.model.prepare_data(self.data_handler.load_data("training"))
-        validation_data = self.model.prepare_data(
-            self.data_handler.load_data("validation")
-        )
-        self.model.train(training_data, validation_data)
+        self.data_handler.check_datasets(db_types=db_types)
+        data = [
+            self.model.prepare_data(self.data_handler.load_datasets(db_type))
+            for db_type in db_types
+        ]
+        self.model.train(*data)
 
