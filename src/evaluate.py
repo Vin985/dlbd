e8984c581f385b9d94f7f729d37fadb1aff56fa9
Vin985 <sylvain.christin@gmail.com>
2020-12-16 20:28:35 -0400

    cleanup

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,19 +5,10 @@ from dlbd.audio.song_detector_evaluator import SongDetectorEvaluator
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-# stream = open("src/evaluation_config.yaml", "r")
-# opts = yaml.load(stream, Loader=yaml.Loader)
-
-# print(opts)
-
-# for model in opts["models"]:
-#     versions = model.get("versions", 1)
-#     print(type(versions))
 
 evaluator = SongDetectorEvaluator(
-    opts_path="src/evaluation_config2.yaml", dh_class=AudioDataHandler
+    opts_path="src/evaluation_config.yaml", dh_class=AudioDataHandler
 )
 
 res = evaluator.evaluate()
 
-# res.to_csv("results/evaluation/stats3.csv")
