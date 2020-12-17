80b42a6df67f32a14e3c49a6a2e6effeeef5c163
Vin985 <sylvain.christin@gmail.com>
2020-12-16 20:31:45 -0400

    load scenarios

--- src/dlbd/training/trainer.py
+++ src/dlbd/training/trainer.py
@@ -1,3 +1,4 @@
+from ..utils import common as common_utils
 from ..utils.model_handler import ModelHandler
 
 
@@ -20,4 +21,24 @@ class Trainer(ModelHandler):
             for db_type in db_types
         ]
         self.model.train(*data)
+
+    def expand_training_scenarios(self):
+        scenarios = []
+        if "scenarios" in self.opts:
+            clean = dict(self.opts)
+            clean.pop("scenarios")
+            for scenario in self.opts["scenarios"]:
+                for opts in common_utils.expand_options_dict(scenario):
+                    res = dict(clean)
+                    res = common_utils.deep_dict_update(res, opts, copy=True)
+                    scenarios.append(res)
+        else:
+            scenarios.append(dict(self.opts))
+        return scenarios
+
+    def load_scenarios(self):
+        return self.expand_training_scenarios()
+
+    def train(self):
+        pass
 
