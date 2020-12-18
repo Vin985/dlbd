b943e1e9f07d041e85f0e8d3cbbc5836c093f862
Vin985 <sylvain.christin@gmail.com>
2020-12-17 20:20:12 -0400

    expand training scenarios

--- src/dlbd/training/trainer.py
+++ src/dlbd/training/trainer.py
@@ -28,7 +28,10 @@ class Trainer(ModelHandler):
             clean = dict(self.opts)
             clean.pop("scenarios")
             for scenario in self.opts["scenarios"]:
-                for opts in common_utils.expand_options_dict(scenario):
+                # TODO: do not expand the dictionaries? Keep the paradigm of 1 scenario, 1 model?
+                for opts in common_utils.expand_options_dict(
+                    scenario  # , exclude_expand=["databases_options"]
+                ):
                     res = dict(clean)
                     res = common_utils.deep_dict_update(res, opts, copy=True)
                     scenarios.append(res)
@@ -39,6 +42,34 @@ class Trainer(ModelHandler):
     def load_scenarios(self):
         return self.expand_training_scenarios()
 
+    def get_db_opts_to_update(self, opts_update, db_name):
+        if isinstance(opts_update, list):
+            print("in list")
+            for opts in opts_update:
+                if opts.get("name", "") == db_name:
+                    return opts
+        else:
+            if opts_update.get("name", "") == db_name:
+                return opts_update
+        return None
+
+    def get_scenario_databases_options(self, scenario):
+        db_opts = []
+        opts_update = scenario.get("databases_options", {})
+        for db_name in scenario["databases"]:
+            db_opt = self.data_handler.update_database(opts_update, db_name)
+            print(db_opt)
+            # if opts_update:
+            #     # new_opts = self.get_db_opts_to_update(opts_update, db_name)
+            #     # if new_opts:
+            #     db_opt = self.data_handler.duplicate_database(opts_update)
+            if db_opt:
+                db_opts.append(db_opt)
+        return db_opts
+
     def train(self):
-        pass
+        # print(self.scenarios)
+        for scenario in self.scenarios:
+            databases = self.get_scenario_databases_options(scenario)
+            # print(scenario)
 
