9e62b4804c07bec36698f1232086aa3b9f1472ee
Vin985 <sylvain.christin@gmail.com>
2020-12-21 20:58:37 -0400

    moved model name, version and root dir to model options

--- src/dlbd/models/dlmodel.py
+++ src/dlbd/models/dlmodel.py
@@ -20,39 +20,39 @@ class DLModel(ABC):
         """Create the layers of the neural network, with the same options we used in training"""
         self.model = None
         self._results_dir = None
-        self._version = None
+        # self._version = None
         self._opts = None
-        self._model_name = ""
-        self.results_dir_root = None
-        self.version = version
+        # self._model_name = ""
+        # self.results_dir_root = None
+        # self.version = version
         if opts:
             self.opts = opts
 
-    @property
-    def model_name(self):
-        if not self._model_name:
-            if self.version is not None:
-                self._model_name = self.NAME + "_v" + str(self.version)
-            else:
-                return self.NAME
-        return self._model_name
+    # @property
+    # def model_name(self):
+    #     if not self._model_name:
+    #         if self.version is not None:
+    #             self._model_name = self.NAME + "_v" + str(self.version)
+    #         else:
+    #             return self.NAME
+    #     return self._model_name
 
-    @property
-    def version(self):
-        if self._version is None:
-            v = self.get_model_version(self.results_dir_root)
-            if self.opts["model"].get("from_epoch", 0) and v > 0:
-                v -= 1
-            self._version = v
-        return self._version
+    # @property
+    # def version(self):
+    #     if self._version is None:
+    #         v = self.get_model_version(self.results_dir_root)
+    #         if self.opts["model"].get("from_epoch", 0) and v > 0:
+    #             v -= 1
+    #         self._version = v
+    #     return self._version
 
-    @version.setter
-    def version(self, version):
-        self._version = version
+    # @version.setter
+    # def version(self, version):
+    #     self._version = version
 
-    @property
-    def results_dir(self):
-        return self.results_dir_root / str(self.version)
+    # @property
+    # def results_dir(self):
+    #     return self.results_dir_root / str(self.version)
 
     @property
     def opts(self):
@@ -62,7 +62,7 @@ class DLModel(ABC):
     def opts(self, opts):
         self._opts = opts
         self.model = self.create_net()
-        self.results_dir_root = Path(self.opts["model_dir"]) / self.NAME
+        # self.results_dir_root = Path(self.opts["model_dir"]) / self.NAME
 
     @abstractmethod
     def create_net(self):
@@ -108,19 +108,19 @@ class DLModel(ABC):
         with open(self.results_dir / "network_opts.yaml", "w") as f:
             yaml.dump(self.opts, f, default_flow_style=False)
 
-    @staticmethod
-    def get_model_version(path):
-        version = 1
-        if path.exists():
-            for item in path.iterdir():
-                if item.is_dir():
-                    try:
-                        res = int(item.name)
-                        if res >= version:
-                            version = res + 1
-                    except ValueError:
-                        continue
-        return version
+    # @staticmethod
+    # def get_model_version(path):
+    #     version = 1
+    #     if path.exists():
+    #         for item in path.iterdir():
+    #             if item.is_dir():
+    #                 try:
+    #                     res = int(item.name)
+    #                     if res >= version:
+    #                         version = res + 1
+    #                 except ValueError:
+    #                     continue
+    #     return version
 
     def save_model(self, path=None):
         self.save_params()
--- src/dlbd/options/model_options.py
+++ src/dlbd/options/model_options.py
@@ -3,11 +3,21 @@ from .options import Options
 
 class ModelOptions(Options):
 
-    DEFAULT_VALUES = {"id": "", "version": 1}
+    DEFAULT_VALUES = {"id": ""}
 
     def __init__(self, opts):
         super().__init__(opts)
-        self._model_name, self._model_id = "", ""
+        self._model_name = ""
+        self._model_id = ""
+        self._version = None
+
+    @property
+    def results_dir_root(self):
+        return self.model_dir / self.name
+
+    @property
+    def results_dir(self):
+        return self.results_dir_root / str(self.version)
 
     @property
     def model_name(self):
@@ -21,6 +31,34 @@ class ModelOptions(Options):
             self._model_id = self.model_name
             mid = self.id
             if mid:
-                self._model_id += "_" + mid
+                self._model_id += "_" + self.resolve_id(mid)
             self.opts["model_id"] = self._model_id
         return self._model_id
+
+    def resolve_id(self, model_id):
+        mid = model_id
+        return mid
+
+    @property
+    def version(self):
+        if self._version is None:
+            v = self.opts.get("version", None)
+            if not v:
+                v = self.get_model_version(self.results_dir_root)
+            self._version = v
+        return self._version
+
+    def get_model_version(self, path):
+        version = 1
+        if path.exists():
+            for item in path.iterdir():
+                if item.is_dir():
+                    try:
+                        res = int(item.name)
+                        if res >= version:
+                            version = res + 1
+                    except ValueError:
+                        continue
+        if self.opts["model"].get("from_epoch", 0) and version > 0:
+            version -= 1
+        return version
