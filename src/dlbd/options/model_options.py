f9b1f297631835e3fa936dbe7a202d4c41c16a8e
Vin985 <sylvain.christin@gmail.com>
2021-01-04 11:48:37 -0400

    id generation for model

--- src/dlbd/options/model_options.py
+++ src/dlbd/options/model_options.py
@@ -1,42 +1,46 @@
+import re
+
 from .options import Options
+from ..utils import common as common_utils
 
 
 class ModelOptions(Options):
 
-    DEFAULT_VALUES = {"id": ""}
+    DEFAULT_VALUES = {"id": "{version}", "id_prefixes": {"version": "_v"}}
 
     def __init__(self, opts):
         super().__init__(opts)
-        self._model_name = ""
         self._model_id = ""
         self._version = None
 
     @property
     def results_dir_root(self):
-        return self.model_dir / self.name
+        return self.model_dir / self.model_id
 
     @property
     def results_dir(self):
         return self.results_dir_root / str(self.version)
 
     @property
-    def model_name(self):
-        if not self._model_name:
-            self._model_name = self.name + "_v" + str(self.version)
-        return self._model_name
-
-    @property
     def model_id(self):
         if not self._model_id:
-            self._model_id = self.model_name
-            mid = self.id
-            if mid:
-                self._model_id += "_" + self.resolve_id(mid)
-            self.opts["model_id"] = self._model_id
+            self._model_id = self.name + self.resolve_id(self.id)
+            # self.opts["model_id"] = self._model_id
         return self._model_id
 
     def resolve_id(self, model_id):
-        mid = model_id
+        prefixes = self.id_prefixes
+        to_replace = re.findall("\\{(.+?)\\}", model_id)
+        res = {}
+        for key in to_replace:
+            mid = ""
+            if prefixes:
+                prefix = prefixes.get(key, prefixes.get("default", ""))
+                mid += str(prefix)
+            mid += str(common_utils.get_dict_path(self.opts, key, key))
+            res[key] = mid
+
+        mid = model_id.format(**res)
         return mid
 
     @property
