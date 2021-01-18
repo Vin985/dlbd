c38db2a7355829410d8c46a59699e5445c073294
Vin985 <sylvain.christin@gmail.com>
2021-01-18 16:47:41 -0400

    split load_file_data in two

--- src/dlbd/data/audio_data_handler.py
+++ src/dlbd/data/audio_data_handler.py
@@ -39,32 +39,44 @@ class AudioDataHandler(DataHandler):
         opts["classes"] = self.load_classes(database)
         return opts
 
-    def load_file_data(self, file_path, tags_dir, opts):
-        # load file and convert to spectrogram
-        tag_opts = opts["tags"]
+    @staticmethod
+    def load_raw_data(file_path, opts, *args, **kwargs):
         spec_opts = opts["spectrogram"]
-        sr = spec_opts.get("sample_rate", None)
+        sr = spec_opts.get("sample_rate", "original")
         if sr and sr == "original":
             sr = None
+        # * NOTE: sample_rate can be different from sr if sr is None
         wav, sample_rate = librosa.load(str(file_path), sr=sr)
+        # * NOTE: sp_opts can contain options not defined in spec_opts
+        spec, sp_opts = spectrogram.generate_spectrogram(wav, sample_rate, spec_opts)
+        return spec, sp_opts, sample_rate, len(wav)
+
+    @staticmethod
+    def load_tags(tags_dir, opts, audio_info, spec_len, *args, **kwargs):
+        tag_opts = opts["tags"]
+        tag_df = tag_manager.get_tag_df(audio_info, tags_dir, tag_opts)
+
+        # dur = len(wav) / sample_rate
+        # print("pps:", spec.shape[1] / dur)
+
+        tmp_tags = tag_manager.filter_classes(tag_df, opts["classes"])
+        tag_presence = tag_manager.get_tag_presence(tmp_tags, audio_info, tag_opts)
+        factor = float(spec_len) / tag_presence.shape[0]
+        zoomed_presence = zoom(tag_presence, factor).astype(int)
+        return tmp_tags, zoomed_presence
+
+    def load_file_data(self, file_path, tags_dir, opts):
+        spec, spec_opts, sr, nframes = self.load_raw_data(file_path, opts)
 
         audio_info = {
             "file_path": file_path,
-            "sample_rate": sample_rate,
-            "length": len(wav),
+            "sample_rate": sr,
+            "length": nframes,
+            "spec_opts": spec_opts,
         }
-        tag_df = tag_manager.get_tag_df(audio_info, tags_dir, tag_opts)
-
-        # * NOTE: sp_opts can contain options not defined in spec_opts
-        spec, sp_opts = spectrogram.generate_spectrogram(wav, sample_rate, spec_opts)
-
-        audio_info["spec_opts"] = sp_opts
-        tmp_tags = tag_manager.filter_classes(tag_df, opts["classes"])
-        tag_presence = tag_manager.get_tag_presence(tmp_tags, audio_info, tag_opts)
-        factor = float(spec.shape[1]) / tag_presence.shape[0]
-        zoomed_presence = zoom(tag_presence, factor).astype(int)
+        tags_df, tags_linear = self.load_tags(tags_dir, opts, audio_info, spec.shape[1])
 
         self.tmp_db_data["spectrograms"].append(spec)
         self.tmp_db_data["infos"].append(audio_info)
-        self.tmp_db_data["tags_df"].append(tmp_tags)
-        self.tmp_db_data["tags_linear_presence"].append(zoomed_presence)
+        self.tmp_db_data["tags_df"].append(tags_df)
+        self.tmp_db_data["tags_linear_presence"].append(tags_linear)
