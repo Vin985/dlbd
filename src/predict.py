a2f2a4c43bb980b9492addb2d589cd06f8f065a8
Vin985 <sylvain.christin@gmail.com>
2021-01-21 16:56:39 -0400

    prediction changes

--- src/predict.py
+++ src/predict.py
@@ -16,12 +16,15 @@ from dlbd.evaluation.song_detector_evaluator import SongDetectorEvaluator
 from dlbd.models.CityNetTF2Dropout import CityNetTF2Dropout
 from dlbd.training.spectrogram_sampler import SpectrogramSampler
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-gpus = tf.config.experimental.list_physical_devices("GPU")
-tf.config.experimental.set_memory_growth(gpus[0], True)
+# gpus = tf.config.experimental.list_physical_devices("GPU")
+# tf.config.experimental.set_memory_growth(gpus[0], True)
 
-src_root = pathlib.Path("/media/vin/Backup/PhD/Acoustics")
+src_raw_root = pathlib.Path("/media/vin/Backup/PhD/Acoustics")
+src_root = pathlib.Path(
+    "/mnt/win/UMoncton/OneDrive - Université de Moncton/Data/Tommy/"
+)
 dest_root = pathlib.Path(
     "/mnt/win/UMoncton/OneDrive - Université de Moncton/Data/Tommy/"
 )
@@ -33,8 +36,6 @@ file_list["plot_id"] = file_list.year.map(str) + "_" + file_list["plot"]
 print(file_list)
 opts = {"n_mels": 32, "type": "mel", "n_fft": 2048, "hop_length": 1024}
 
-#%%
-
 
 #%%
 for plot_id in file_list.plot_id.unique():
@@ -44,7 +45,7 @@ for plot_id in file_list.plot_id.unique():
         tmp = {"specs": [], "infos": []}
         tmp_df = file_list.loc[file_list.plot_id == plot_id]
         for row in tmp_df.itertuples():
-            path = src_root / row.path
+            path = src_raw_root / row.path
             print("Loading file: ", path)
             spec, info = AudioDataHandler.load_raw_data(path, opts)
             tmp["specs"].append(spec)
@@ -80,21 +81,30 @@ i = 0
 for plot_id in file_list.plot_id.unique():
     tic = time()
     plot_tmp = []
-    pkl_path = dest_root / (plot_id + ".pkl")
+    pkl_path = src_root / (plot_id + ".pkl")
     data = pickle.load(open(pkl_path, "rb"))
     for i, spec in enumerate(data["specs"]):
         tmp = SongDetectorEvaluator.classify_element(
             model, spec, data["infos"][i], sampler
         )
         plot_tmp.append(tmp)
+        break
     tmp_df = pd.concat(plot_tmp).reset_index()
     res.append(tmp_df)
-    tmp_df.to_feather(str(dest_root) + "/intermediate/" + plot_id + ".feather")
+    tmp_df.to_feather(
+        str(
+            ensure_path_exists(
+                dest_root / "intermediate" / (plot_id + ".feather"), is_file=True
+            )
+        )
+    )
     print("Classified {0} in {1}".format(plot_id, time() - tic))
 
 
 res_df = pd.concat(res).reset_index()
-res_df.to_feather(str(dest_root) + "predictions_all.feather")
+res_df.to_feather(
+    str(ensure_path_exists(dest_root / "predictions_all.feather", is_file=True))
+)
 
 # #%%
 
