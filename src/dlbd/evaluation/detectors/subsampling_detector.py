67fbf47cc3bbeea3c6b141019eb3ff4353d9ee75
Vin985 <sylvain.christin@gmail.com>
2021-01-21 16:56:55 -0400

    modified detectors to detect events without tags

--- src/dlbd/evaluation/detectors/subsampling_detector.py
+++ src/dlbd/evaluation/detectors/subsampling_detector.py
@@ -85,22 +85,11 @@ class SubsamplingDetector(Detector):
         events = pd.DataFrame(events)
         return events
 
-    def get_events(self, predictions, tags, options):
-        events = predictions.groupby(
-            "recording_id", as_index=True, observed=True
-        ).apply(self.get_recording_events, tags, options)
-        return events
-
-    @staticmethod
-    def list_append(x, y):
-        x.append(y)
-
-    def get_recording_events(self, predictions, tags, options=None):
-        options = options or {}
-        tags = tags[tags.recording_id == predictions.name]
+    def get_recording_events(self, predictions, options, tags=None):
         threshold = options.get("event_threshold", self.DEFAULT_EVENT_THRESHOLD)
         predictions.loc[predictions.activity > threshold, "event"] = 1
-        if not predictions.empty:
+        if not predictions.empty and tags is not None:
+            tags = tags[tags.recording_id == predictions.name]
             step = predictions.time.values[1] - predictions.time.values[0]
             for tag in tags.itertuples():
                 start = math.ceil(tag.tag_start / step)
@@ -110,26 +99,64 @@ class SubsamplingDetector(Detector):
 
         return predictions
 
-    def match_events_apply(self, predictions, tags, options):
+    def match_events_apply(self, predictions, options, tags=None):
         recording_id = predictions.name
-        current_tags = tags.loc[tags.recording_id == recording_id]
         threshold = options.get("event_threshold", self.DEFAULT_EVENT_THRESHOLD)
         step = options.get("sample_step", self.DEFAULT_MIN_DURATION) * 1000
         resampler = predictions.resample(str(step) + "ms") if step else predictions
         resample_func = functools.partial(self.resample_max, threshold=threshold)
-        tmp_tags = {
-            "id": current_tags.id.to_numpy().astype(str),
-            "start": current_tags.tag_start.to_numpy(),
-            "end": current_tags.tag_end.to_numpy(),
+        agg_funcs = {
+            "activity": resample_func,
         }
-        tag_func = functools.partial(self.get_tag_index, step=step, tags=tmp_tags)
+        if tags is not None:
+            current_tags = tags.loc[tags.recording_id == recording_id]
+            tmp_tags = {
+                "id": current_tags.id.to_numpy().astype(str),
+                "start": current_tags.tag_start.to_numpy(),
+                "end": current_tags.tag_end.to_numpy(),
+            }
+            tag_func = functools.partial(self.get_tag_index, step=step, tags=tmp_tags)
+            agg_funcs["tag_index"] = tag_func
         res = (
-            resampler.agg({"activity": resample_func, "tag_index": tag_func})
+            resampler.agg(agg_funcs)
             .reset_index()
             .rename(columns={"activity": "event", "datetime": "time"})
         )
-        # res["recording_id"] = recording_id
         return res
+
+    def get_events(self, predictions, options=None, tags=None, *args, **kwargs):
+        options = options or {}
+        preds = predictions.copy()
+        preds.loc[:, "event"] = 0
+        preds = preds.astype({"event": np.int8})
+        preds.recording_id = preds.recording_id.astype("category")
+
+        if tags is not None:
+            preds.loc[:, "tag_index"] = pd.Series([[] for i in range(preds.shape[0])])
+
+        step = options.get("sample_step", self.DEFAULT_MIN_DURATION) * 1000
+
+        if step:
+            preds.loc[:, "datetime"] = pd.to_datetime(preds.time * 10 ** 9)
+            preds = preds.drop(columns=["time"])
+            preds.set_index("datetime", inplace=True)
+            apply_func = self.match_events_apply
+        else:
+            apply_func = self.get_recording_events
+
+        events = (
+            preds.groupby("recording_id", observed=True)
+            .apply(apply_func, options=options, tags=tags)
+            .reset_index()
+        )
+        if tags is not None:
+            events.loc[:, "tag"] = 0
+            events.loc[events.tag_index.str.len() > 0, "tag"] = 2
+        return events
+
+    @staticmethod
+    def list_append(x, y):
+        x.append(y)
 
     def recording_plot(self, matches, options, tags_presence, step):
 
@@ -177,7 +204,7 @@ class SubsamplingDetector(Detector):
         res = df.tag + df.event
 
         n_true_positives = len(res[res == 3])
-        n_true_negatives = len(res[res == 0])
+        # n_true_negatives = len(res[res == 0])
         n_false_positives = len(res[res == 1])
         n_false_negatives = len(res[res == 2])
 
@@ -216,32 +243,8 @@ class SubsamplingDetector(Detector):
 
     def evaluate(self, predictions, tags, options):
         tags_df = tags["tags_df"]
-        # tags_presence = {}
-        # for idx, i in enumerate(tags["tags_linear_presence"]):
-        #     fp = str(tags["infos"][idx]["file_path"])
-        #     tags_presence[fp] = i
 
-        preds = predictions.copy()
-        # preds.loc[:, "tag_index"] = ""
-        preds.loc[:, "tag_index"] = pd.Series([[] for i in range(preds.shape[0])])
-        preds.loc[:, "event"] = 0
-        preds.recording_id = preds.recording_id.astype("category")
-        step = options.get("sample_step", self.DEFAULT_MIN_DURATION) * 1000
-
-        if step:
-            preds.loc[:, "datetime"] = pd.to_datetime(preds.time * 10 ** 9)
-            preds.set_index("datetime", inplace=True)
-            apply_func = self.match_events_apply
-        else:
-            apply_func = self.get_recording_events
-
-        events = (
-            preds.groupby("recording_id", observed=True)
-            .apply(apply_func, tags=tags_df, options=options)
-            .reset_index()
-        )
-        events.loc[:, "tag"] = 0
-        events.loc[events.tag_index.str.len() > 0, "tag"] = 2
+        events = self.get_events(predictions, options, tags_df)
 
         stats = self.get_stats(events, tags_df)
         print("Stats for options {0}: {1}".format(options, stats))
