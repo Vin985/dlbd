b50011a59d743880c51493b92c17bd9e7b883fc9
Vin985 <sylvain.christin@gmail.com>
2021-01-27 16:55:36 -0400

    changed splits

--- src/dlbd/utils/split.py
+++ src/dlbd/utils/split.py
@@ -1,32 +1,19 @@
-from mouffet.utils.split import split_folder
+import pandas as pd
+from mouffet.utils.file import list_folder
 
 
-def arctic_split(paths, data_handler=None, database=None):
-    audio_path = paths["audio"]["training"]
-    if not audio_path.exists():
-        raise ValueError(
-            "'audio_dir' option must be provided to split into test, training and"
-            + "validation subsets"
-        )
-    split = data_handler.get_db_option("split", database, None)
-    if not split:
-        raise ValueError("Split option must be provided for arctic split function")
-    split_props = []
-    # * Make test split optional
-    test_split = split.get("test", 0)
-    if test_split:
-        split_props.append(test_split)
-    val_split = split.get("validation", 0.2)
-    split_props.append(val_split)
-    splits = split_folder(audio_path, split_props)
-    res = {}
-    idx = 0
-    if test_split:
-        res["test"] = splits[idx]
-        idx += 1
-    res["validation"] = splits[idx]
-    res["training"] = splits[idx + 1]
-
-    print([(k + " " + str(len(v))) for k, v in res.items()])
-    return res
-
+def citynet_split(path, split_props, extensions):
+    splits = [[] for i in range(len(split_props) + 1)]
+    _, files = list_folder(path, extensions)
+    file_names = [path.stem for path in files]
+    groups = [name.split("_")[0] for name in file_names]
+    df = pd.DataFrame({"paths": files, "groups": groups})
+    tmp_df = df.copy()
+    i = 0
+    for split in split_props:
+        split_df = tmp_df.groupby("groups").sample(frac=split)
+        splits[i] = split_df.paths.to_list()
+        tmp_df = tmp_df[~tmp_df.isin(split_df)].dropna()
+        i += 1
+    splits[i] = tmp_df.paths.to_list()
+    return None
