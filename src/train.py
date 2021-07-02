f5f263a404f1f8f602d8d00f3618c9b04fdd837e
Vin985 <sylvain.christin@gmail.com>
2021-07-02 15:42:51 -0300

    various changes

--- src/train.py
+++ src/train.py
@@ -12,7 +12,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 # tf.config.experimental.set_memory_growth(gpus[0], True)
 
 trainer = Trainer(
-    opts_path="src/CityNetTF2_Dropout_training_config.yaml",
+    opts_path="src/training_config_test.yaml",
     # opts_path="CityNetTF2_Dropout_training_config.yaml",
     # model_class=DLBDLite,
     dh_class=AudioDataHandler,
