1dfcec766d37f2578b217ec91e4bf22119a9932f
Vin985 <sylvain.christin@gmail.com>
2021-08-13 16:21:18 -0300

    updated dlbdlite

--- src/dlbd/models/dlbd.py
+++ src/dlbd/models/dlbd.py
@@ -66,59 +66,6 @@ class DLBDDense(CityNetTF2):
         x = layers.Dense(2, activation=None, name="fc8")(x)
         return x
 
-    def add_layers(self, inputs):
-        x = layers.Conv2D(
-            self.opts.get("num_filters", 128),
-            (
-                self.opts["spec_height"] - self.opts["wiggle_room"],
-                self.opts["conv_filter_width"],
-            ),
-            bias_initializer=None,
-            padding="valid",
-            activation=None,
-            kernel_regularizer=regularizers.l2(0.001),
-        )(inputs)
-        # * Second block
-        x = layers.Conv2D(
-            self.opts.get("num_filters", 128),
-            (1, 3),
-            bias_initializer=None,
-            padding="valid",
-            activation=None,
-            kernel_regularizer=regularizers.l2(0.001),
-        )(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.LeakyReLU(alpha=1 / 3, name="conv1_2",)(x)
-        W = x.shape[2]
-        print(x.shape)
-        print(W)
-        x = layers.MaxPool2D(pool_size=(1, W), strides=(1, 1), name="pool2")(x)
-        x = layers.Dropout(0.5)(x)
-        x = tf.transpose(x, (0, 3, 2, 1))
-        x = layers.Flatten(name="pool2_flat")(x)
-        x = layers.Dense(
-            self.opts["num_dense_units"],
-            activation=None,
-            bias_initializer=None,
-            kernel_regularizer=regularizers.l2(0.001),
-        )(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.LeakyReLU(alpha=1 / 3, name="fc6")(x)
-        x = layers.Dropout(0.5)(x)
-        x = layers.Dense(
-            self.opts["num_dense_units"],
-            activation=None,
-            bias_initializer=None,
-            # kernel_regularizer=regularizers.l2(0.001),
-        )(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.LeakyReLU(alpha=1 / 3, name="fc7")(x)
-        self.base_model = layers.Dropout(0.5)(x)
-        self.top_layers = layers.Dense(
-            2, activation=None, name="fc8"
-        )  # kernel_regularizer=regularizers.l2(0.001),
-        return self.top_layers(x)
-
 
 class DLBDLite(CityNetTF2):
     """DLBD Network with one less Dense layer to reduce the number of parameters and overfitting
@@ -132,7 +79,10 @@ class DLBDLite(CityNetTF2):
 
     NAME = "DLBD_lite"
 
-    def add_layers(self, inputs):
+    def get_base_layers(self, x=None):
+        if x is None:
+            x = self.get_preprocessing_layers()
+        # * First block
         x = layers.Conv2D(
             self.opts.get("num_filters", 128),
             (
@@ -143,7 +93,7 @@ class DLBDLite(CityNetTF2):
             padding="valid",
             activation=None,
             kernel_regularizer=regularizers.l2(0.001),
-        )(inputs)
+        )(x)
         # * Second block
         x = layers.Conv2D(
             self.opts.get("num_filters", 128),
@@ -168,9 +118,11 @@ class DLBDLite(CityNetTF2):
         )(x)
         x = layers.BatchNormalization()(x)
         x = layers.LeakyReLU(alpha=1 / 3, name="fc6")(x)
-        x = layers.Dropout(0.5)(x)
-        outputs = layers.Dense(
-            2, activation=None, name="fc8"  # kernel_regularizer=regularizers.l2(0.001),
-        )(x)
-        return outputs
+        return x
+
+    def get_top_layers(self, x=None):
+        if x is None:
+            x = self.get_base_layers()
+        x = layers.Dense(2, activation=None, name="fc8")(x)
+        return x
 
