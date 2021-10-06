875f31e6dd4c2b977117a83b71a7ad177f635123
Vin985 <sylvain.christin@gmail.com>
2021-10-05 21:05:39 -0300

    use new config layout

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,15 +5,16 @@ from dlbd.evaluation.song_detector_evaluation_handler import (
 )
 
 import pandas as pd
-
+import tensorflow as tf
 
 pd.options.mode.chained_assignment = "raise"
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+gpus = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
 evaluator = SongDetectorEvaluationHandler(
-    opts_path="src/evaluation_config_new.yaml", dh_class=AudioDataHandler
+    opts_path="config/evaluation/run1/evaluation_config.yaml", dh_class=AudioDataHandler
 )
 
 res = evaluator.evaluate()
--- src/train.py
+++ src/train.py
@@ -1,18 +1,19 @@
 import os
+import logging
 
 # import tensorflow as tf
 from mouffet.training.training_handler import TrainingHandler
 
 from dlbd.data.audio_data_handler import AudioDataHandler
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 # gpus = tf.config.experimental.list_physical_devices("GPU")
 # tf.config.experimental.set_memory_growth(gpus[0], True)
 
+logging.basicConfig(level=logging.DEBUG)
+
 trainer = TrainingHandler(
-    opts_path="src/training_config_test.yaml",
-    # opts_path="CityNetTF2_Dropout_training_config.yaml",
-    # model_class=DLBDLite,
+    opts_path="config/training/run1/training_config.yaml",
     dh_class=AudioDataHandler,
 )
 trainer.train()
