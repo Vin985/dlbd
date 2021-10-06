c53f3e11fe3f977df280f67c085318124db85278
Vin985 <sylvain.christin@gmail.com>
2021-10-06 13:09:35 -0300

    change config layout again

--- src/evaluate.py
+++ src/evaluate.py
@@ -14,7 +14,7 @@ gpus = tf.config.experimental.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
 evaluator = SongDetectorEvaluationHandler(
-    opts_path="config/evaluation/run1/evaluation_config.yaml", dh_class=AudioDataHandler
+    opts_path="config/run1/evaluation_config.yaml", dh_class=AudioDataHandler
 )
 
 res = evaluator.evaluate()
