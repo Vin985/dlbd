3159570266b7ab67ca39cfe80940748bf4f6974e
Vin985 <sylvain.christin@gmail.com>
2021-10-29 13:47:08 -0300

    new models

--- src/dlbd/models/dlbd.py
+++ src/dlbd/models/dlbd.py
@@ -260,7 +260,7 @@ class DLBDLiteBNrelu(CityNetTF2):
         return x
 
 
-class DLBDDenseBNrelu(CityNetTF2):
+class DLBDDenseBNRelu(CityNetTF2):
     """DLBD Network with one less Dense layer to reduce the number of parameters and overfitting
 
     Args:
@@ -304,14 +304,14 @@ class DLBDDenseBNrelu(CityNetTF2):
         x = tf.transpose(x, (0, 3, 2, 1))
         x = layers.Flatten(name="pool2_flat")(x)
         x = layers.Dense(
-            256,
+            self.opts["num_dense_units"],
             activation="relu",
             bias_initializer=None,
             kernel_regularizer=regularizers.l2(0.001),
         )(x)
         x = layers.BatchNormalization()(x)
         x = layers.Dense(
-            32,
+            self.opts["num_dense_units2"],
             activation="relu",
             bias_initializer=None,
             # kernel_regularizer=regularizers.l2(0.001),
@@ -384,6 +384,71 @@ class DLBDLiteDrop(CityNetTF2):
         )(x)
         x = layers.LeakyReLU(alpha=1 / 3, name="fc6")(x)
         x = layers.Dropout(0.5)(x)
+        return x
+
+    def get_top_layers(self, x=None):
+        if x is None:
+            x = self.get_base_layers()
+        x = layers.Dense(2, activation=None, name="fc8")(x)
+        return x
+
+
+class DLBDDenseDropRelu(CityNetTF2):
+    """DLBD Network with one less Dense layer to reduce the number of parameters and overfitting
+
+    Args:
+        CityNetTF2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    NAME = "DLBD_lite"
+
+    def get_base_layers(self, x=None):
+        if x is None:
+            x = self.get_preprocessing_layers()
+        # * First block
+        x = layers.Conv2D(
+            self.opts.get("num_filters", 128),
+            (
+                self.opts["input_height"] - self.opts["wiggle_room"],
+                self.opts["conv_filter_width"],
+            ),
+            bias_initializer=None,
+            padding="valid",
+            activation="relu",
+            kernel_regularizer=regularizers.l2(0.001),
+        )(x)
+        # * Second block
+        x = layers.Conv2D(
+            self.opts.get("num_filters", 128),
+            (1, 3),
+            bias_initializer=None,
+            padding="valid",
+            activation="relu",
+            kernel_regularizer=regularizers.l2(0.001),
+        )(x)
+        W = x.shape[2]
+        x = layers.MaxPool2D(pool_size=(1, W), strides=(1, 1), name="pool2")(x)
+        x = layers.Dropout(0.5)(x)
+        x = tf.transpose(x, (0, 3, 2, 1))
+        x = layers.Flatten(name="pool2_flat")(x)
+        x = layers.Dense(
+            self.opts["num_dense_units"],
+            activation="relu",
+            bias_initializer=None,
+            kernel_regularizer=regularizers.l2(0.001),
+        )(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(
+            self.opts["num_dense_units2"],
+            activation="relu",
+            bias_initializer=None,
+            kernel_regularizer=regularizers.l2(0.001),
+        )(x)
+        x = layers.Dropout(0.5)(x)
+
         return x
 
     def get_top_layers(self, x=None):
