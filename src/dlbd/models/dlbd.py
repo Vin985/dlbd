92d15c2fafad65ebae1896059299d9335045680d
Vin985 <sylvain.christin@gmail.com>
2021-11-01 16:02:08 -0300

    models

--- src/dlbd/models/dlbd.py
+++ src/dlbd/models/dlbd.py
@@ -381,6 +381,64 @@ class DLBDLiteBNrelu(CityNetTF2):
         return x
 
 
+class DLBDLiteBNreluNoreg(CityNetTF2):
+    """DLBD Network with one less Dense layer to reduce the number of parameters and overfitting
+
+    Args:
+        CityNetTF2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    NAME = "DLBD_lite"
+
+    def get_base_layers(self, x=None):
+        if x is None:
+            x = self.get_preprocessing_layers()
+        # * First block
+        x = layers.Conv2D(
+            self.opts.get("num_filters", 128),
+            (
+                self.opts["input_height"] - self.opts["wiggle_room"],
+                self.opts["conv_filter_width"],
+            ),
+            bias_initializer=None,
+            padding="valid",
+            activation="relu",
+        )(x)
+        x = layers.BatchNormalization()(x)
+        # * Second block
+        x = layers.Conv2D(
+            self.opts.get("num_filters", 128),
+            (1, 3),
+            bias_initializer=None,
+            padding="valid",
+            activation="relu",
+        )(x)
+        x = layers.BatchNormalization()(x)
+        W = x.shape[2]
+        x = layers.MaxPool2D(pool_size=(1, W), strides=(1, 1), name="pool2")(x)
+        x = layers.BatchNormalization()(x)
+        x = tf.transpose(x, (0, 3, 2, 1))
+        x = layers.Flatten(name="pool2_flat")(x)
+        x = layers.Dense(
+            self.opts["num_dense_units"],
+            activation="relu",
+            bias_initializer=None,
+            kernel_regularizer=regularizers.l2(0.001),
+        )(x)
+        x = layers.BatchNormalization()(x)
+
+        return x
+
+    def get_top_layers(self, x=None):
+        if x is None:
+            x = self.get_base_layers()
+        x = layers.Dense(2, activation=None, name="fc8")(x)
+        return x
+
+
 class DLBDDenseBNRelu(CityNetTF2):
     """DLBD Network with one less Dense layer to reduce the number of parameters and overfitting
 
