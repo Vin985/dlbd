f7a7c407b1e039caf80b9277cfdd02dcb55b9819
Vin985 <sylvain.christin@gmail.com>
2021-11-05 13:02:33 -0300

    removed useless test

--- src/dlbd/models/layers.py
+++ src/dlbd/models/layers.py
@@ -85,22 +85,17 @@ class MaskSpectrograms(tf.keras.layers.Layer):
         self.freq_mask = freq_mask
 
     @tf.function(experimental_relax_shapes=True)
-    def mask(self, spec, training=None):
-        if training:
-            if self.time_mask:
-                if tf.random.uniform(  # pylint: disable=unexpected-keyword-arg
-                    shape=(), minval=1, maxval=100, dtype=tf.int32
-                ) > self.time_mask.get("prop", 70):
-                    spec = tfio.audio.time_mask(
-                        spec, param=self.time_mask.get("value", 10)
-                    )
-            if self.freq_mask:
-                if tf.random.uniform(  # pylint: disable=unexpected-keyword-arg
-                    shape=(), minval=1, maxval=100, dtype=tf.int32
-                ) > self.freq_mask.get("prop", 70):
-                    spec = tfio.audio.freq_mask(
-                        spec, param=self.freq_mask.get("prop", 30)
-                    )
+    def mask(self, spec):
+        if self.time_mask:
+            if tf.random.uniform(  # pylint: disable=unexpected-keyword-arg
+                shape=(), minval=1, maxval=100, dtype=tf.int32
+            ) > self.time_mask.get("prop", 70):
+                spec = tfio.audio.time_mask(spec, param=self.time_mask.get("value", 10))
+        if self.freq_mask:
+            if tf.random.uniform(  # pylint: disable=unexpected-keyword-arg
+                shape=(), minval=1, maxval=100, dtype=tf.int32
+            ) > self.freq_mask.get("prop", 70):
+                spec = tfio.audio.freq_mask(spec, param=self.freq_mask.get("prop", 30))
         return spec
 
     @tf.function
