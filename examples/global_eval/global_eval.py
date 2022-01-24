9cfad29282abeb59bbe0b98647e6c49fa51a5c9f
Vin985 <sylvain.christin@gmail.com>
2022-01-24 15:55:57 -0400

    global eval changes

--- examples/global_eval/global_eval.py
+++ examples/global_eval/global_eval.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 from pathlib import Path
+import pandas as pd
 import mouffet.utils.file as file_utils
 from dlbd.applications.phenology.utils import score_models
 from dlbd.utils import get_models_conf
@@ -22,26 +23,46 @@ evaluation_config_path = (
 )
 
 evaluation_config = file_utils.load_config(evaluation_config_path)
+evaluation_config["save_use_date_subfolder"] = False
+evaluation_config["save_use_time_prefix"] = False
+
+res_dir = Path(evaluation_config["evaluation_dir"])
+if not res_dir:
+    raise ValueError(
+        "You should specify a evaluation_dir folder in the evaluation_config file"
+        + "for global evaluation"
+    )
+
+print(res_dir)
+
+global_evaluator = SongDetectorEvaluationHandler(
+    opts=evaluation_config, dh_class=AudioDataHandler
+)
+global_evaluator.opts["id"] = "global"
 
 res = []
 
 for path in runs_path.iterdir():
     if path.is_dir():
-        conf = deepcopy(evaluation_config)
-        model_dir = path / "models"
-        preds_dir = path / "predictions"
-        conf["predictions_dir"] = preds_dir
-        conf["id"] = path.name
-        print(model_dir)
-        model_opts = {"model_dir": model_dir}
-        conf = get_models_conf(conf, model_opts, append=True)
-        evaluator = SongDetectorEvaluationHandler(opts=conf, dh_class=AudioDataHandler)
-        stats = evaluator.evaluate()
+        save_path = res_dir / (path.name + "_stats.csv")
+        if save_path.exists():
+            stats = []
+            stats.append({"stats": pd.read_csv(save_path)})
+        else:
+            conf = deepcopy(evaluation_config)
+            model_dir = path / "models"
+            preds_dir = path / "predictions"
+            conf["predictions_dir"] = preds_dir
+            conf["id"] = path.name
+            print(model_dir)
+            model_opts = {"model_dir": model_dir}
+            conf = get_models_conf(conf, model_opts, append=True)
+            evaluator = SongDetectorEvaluationHandler(
+                opts=evaluation_config, dh_class=AudioDataHandler
+            )
+            stats = evaluator.evaluate()
         res += stats
 
 # TODO : Matches consolidation does not work
-
-evaluator.opts["id"] = "global"
-paths = evaluator.save_results(res)
-
-score_models(paths["stats"], evaluation_config["evaluation_dir"])
+paths = global_evaluator.save_results(res)
+score_models(paths["stats"], res_dir)
