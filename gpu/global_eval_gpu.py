cfbf7739478d20d92f7d5b5c2490ae1b702228c8
Vin985 <sylvain.christin@gmail.com>
2022-01-24 16:23:46 -0400

    save intermediary results

--- gpu/global_eval_gpu.py
+++ gpu/global_eval_gpu.py
@@ -39,30 +39,36 @@ global_evaluator = SongDetectorEvaluationHandler(
     opts=evaluation_config, dh_class=AudioDataHandler
 )
 global_evaluator.opts["id"] = "global"
+global_res_filename = "global_stats.csv"
+global_res_path = res_dir / global_res_filename
+overwrite = evaluation_config.get("overwrite_results", False)
+
 
 res = []
-
-for path in runs_path.iterdir():
-    if path.is_dir():
-        save_path = res_dir / (path.name + "_stats.csv")
-        if save_path.exists():
-            stats = []
-            stats.append({"stats": pd.read_csv(save_path)})
-        else:
-            conf = deepcopy(evaluation_config)
-            model_dir = path / "models"
-            preds_dir = path / "predictions"
-            conf["predictions_dir"] = preds_dir
-            conf["id"] = path.name
-            print(model_dir)
-            model_opts = {"model_dir": model_dir}
-            conf = get_models_conf(conf, model_opts, append=True)
-            evaluator = SongDetectorEvaluationHandler(
-                opts=evaluation_config, dh_class=AudioDataHandler
-            )
-            stats = evaluator.evaluate()
-        res += stats
+if not global_res_path.exists() or overwrite:
+    print("No global file saved or overwrite is selected, creating it")
+    for path in runs_path.iterdir():
+        if path.is_dir():
+            save_path = res_dir / (path.name + "_stats.csv")
+            if save_path.exists() and not overwrite:
+                stats = []
+                stats.append({"stats": pd.read_csv(save_path)})
+            else:
+                conf = deepcopy(evaluation_config)
+                model_dir = path / "models"
+                preds_dir = path / "predictions"
+                conf["predictions_dir"] = preds_dir
+                conf["id"] = path.name
+                print(model_dir)
+                model_opts = {"model_dir": model_dir}
+                conf = get_models_conf(conf, model_opts, append=True)
+                evaluator = SongDetectorEvaluationHandler(
+                    opts=conf, dh_class=AudioDataHandler
+                )
+                stats = evaluator.evaluate()
+            res += stats
+    filenames = global_evaluator.save_results(res)
+    global_res_filename = filenames["stats"]
 
 # TODO : Matches consolidation does not work
-paths = global_evaluator.save_results(res)
-score_models(paths["stats"], res_dir)
+score_models(global_res_filename, res_dir)
