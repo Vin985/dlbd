8530cf8ec3e8dd54e5060d81054ca2eb62cad103
Vin985 <sylvain.christin@gmail.com>
2022-08-29 12:03:51 -0300

    label plotting

--- src/dlbd/utils/plot_utils.py
+++ src/dlbd/utils/plot_utils.py
@@ -2,7 +2,10 @@ import datetime
 
 
 def format_date_short(dates):
-    return [x.strftime("%d-%m") for x in dates]
+    if (dates[len(dates) - 1] - dates[0]).days > 1:
+        return [x.strftime("%d-%m") for x in dates]
+    else:
+        return [x.strftime("%H:%M") for x in dates]
 
 
 def label_date_from_julian(dates):
