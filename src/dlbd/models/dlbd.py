c3d01986140ccde997f88afdeba360fa64ca3221
Vin985 <sylvain.christin@gmail.com>
2023-01-18 21:16:52 -0400

    change top layers to include last dense layer

--- src/dlbd/models/dlbd.py
+++ src/dlbd/models/dlbd.py
@@ -76,9 +76,16 @@ class DLBD(AudioDetector):
             bias_initializer=None,
             kernel_regularizer=regularizer,
         )(x)
+
+        return x
+
+    def get_top_layers(self, x=None):
+        if x is None:
+            x = self.get_base_layers()
         x = layers.BatchNormalization()(x)
         dense2 = self.opts.get("num_dense_units2", 0)
         if dense2 > 0:
+            regularizer = self.get_regularizer()
             x = layers.Dense(
                 dense2,
                 activation="relu",
@@ -86,12 +93,6 @@ class DLBD(AudioDetector):
                 kernel_regularizer=regularizer,
             )(x)
             x = layers.BatchNormalization()(x)
-
-        return x
-
-    def get_top_layers(self, x=None):
-        if x is None:
-            x = self.get_base_layers()
         x = layers.Dense(2, activation=None, name="fc8")(x)
         return x
 
