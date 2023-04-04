71fe70093fba59682fb0f172398b11e869c6b459
Vin985 <sylvain.christin@gmail.com>
2023-04-04 14:51:53 -0300

    cleanup

--- src/dlbd/data/audio_data_handler.py
+++ src/dlbd/data/audio_data_handler.py
@@ -2,23 +2,12 @@ import numpy as np
 import pandas as pd
 from mouffet import common_utils
 from mouffet.data import DataHandler
-from mouffet.data.split import split_folder
 from scipy.ndimage.interpolation import zoom
 
 from ..data.audio_utils import resize_spectrogram
 from ..options import AudioDatabaseOptions
-from . import tag_utils
 from .audio_database import AudioDatabase
 from .audio_dataset import AudioDataset
-from .split import citynet_split
-
-# class AudioDataStructure(DataStructure):
-#     STRUCTURE = {
-#         "spectrograms": {"type": "data", "data_type": []},
-#         "tags_df": {"type": "tags"},
-#         "tags_linear_presence": {"type": "tags"},
-#         "infos": {"type": "data"},
-#     }
 
 
 class AudioDataHandler(DataHandler):
@@ -28,12 +17,6 @@ class AudioDataHandler(DataHandler):
     DATASET = AudioDataset
 
     DATABASE_CLASS = AudioDatabase
-
-    # DATA_STRUCTURE = AudioDataStructure()
-
-    # DATA_LOADERS = {"default": AudioDataLoader, "bad_challenge": BADChallengeDataLoader}
-
-    SPLIT_FUNCS = {"arctic": split_folder, "citynet": citynet_split}
 
     def __init__(self, opts):
         super().__init__(opts)
@@ -106,36 +89,6 @@ class AudioDataHandler(DataHandler):
         resize_width = int(pix_in_sec * infos["duration"])
         return resize_width
 
-    # def get_spectrogram_subfolder_path(self, database, folder_opts=None):
-    #     if folder_opts is None:
-    #         folder_opts = self.get_subfolder_options(database, "spectrogram")
-    #     return self.get_spec_subfolder(database.spectrogram, folder_opts)
-
-    # @staticmethod
-    # def get_subfolder_option_value(opt, opts, default, prefixes):
-    #     return prefixes.get(opt, "") + str(opts.get(opt, default[opt]))
-
-    # def get_spec_subfolder(self, spec_opts, folder_opts):
-
-    #     opts = folder_opts.get(
-    #         "options", ["sample_rate", "type", "n_fft", "win_length", "hop_length"]
-    #     )
-    #     prefixes = folder_opts.get("prefixes", {})
-    #     tmp = []
-    #     for opt in opts:
-    #         prefix = str(prefixes.get(opt, ""))
-    #         value = str(spec_opts.get(opt, audio_utils.DEFAULT_SPEC_OPTS[opt]))
-    #         if opt == "type":
-    #             if value == "mel":
-    #                 value += str(
-    #                     spec_opts.get("n_mels", audio_utils.DEFAULT_SPEC_OPTS[opt])
-    #                 )
-
-    #         tmp.append(prefix + value)
-
-    #     spec_folder = "_".join(tmp)
-    #     return spec_folder
-
     def merge_datasets(self, datasets):
         merged = super().merge_datasets(datasets)
         merged.data["tags_df"] = pd.concat(merged.data["tags_df"])
@@ -173,49 +126,3 @@ class AudioDataHandler(DataHandler):
             "raw_df": df,
         }
         return tmp_res
-
-    # def get_db_tags_summary(self, database, db_types=None, filter_classes=False):
-    #     res = {}
-    #     if isinstance(database, str):
-    #         database = self.databases[database]
-    #     db_types = db_types or database.db_types
-
-    #     paths = self.get_database_paths(database)
-    #     file_lists = self.check_file_lists(database, paths, db_types)
-
-    #     print("Generating tags summary for database: {}".format(database["name"]))
-
-    #     res = {}
-    #     tmp_all = []
-    #     # * Only load data if the give db_type is in the database definition
-    #     for db_type in db_types:
-    #         if not db_type in database.db_types:
-    #             continue
-
-    #         tmp_tags = []
-    #         tags_dir = paths["tags"][db_type]
-    #         for file_path in file_lists[db_type]:
-    #             tmp_df = tag_utils.get_tag_df(file_path, tags_dir, database.tags)
-    #             if filter_classes:
-    #                 tmp_df = tag_utils.filter_classes(tmp_df, database)
-
-    #             tmp_tags.append(tmp_df)
-
-    #         tmp_df = pd.concat(tmp_tags)
-    #         tmp_df, tmp_summary = self.summarize_tags(tmp_df)
-    #         res[db_type] = {"raw": tmp_df, "summary": tmp_summary}
-    #         tmp_all.append(tmp_df)
-
-    #     all_df = pd.concat(tmp_all)
-    #     all_df, all_summary = self.summarize_tags(all_df)
-    #     res["all"] = {"raw": all_df, "summary": all_summary}
-
-    #     return res
-
-    # def get_databases_summary(self, databases, *args, **kwargs):
-    #     res = {}
-    #     databases = databases or self.databases.values()
-    #     # * Iterate over databases
-    #     for database in databases:
-    #         res[database["name"]] = self.get_db_tags_summary(database, *args, **kwargs)
-    #     return res
--- src/dlbd/data/audio_database.py
+++ src/dlbd/data/audio_database.py
@@ -6,15 +6,9 @@ from mouffet.data.split import split_folder
 
 class AudioDatabase(Database):
 
-    # DEFAULT_VALUES.update(
-    #     {
-    #         "class_type": "biotic",
-    #         "data_extensions": [".wav"],
-    #         "classes_file": "classes.csv",
-    #         "tags_suffix": "-sceneRect.csv",
-    #     }
-    # )
-
     DATASET = AudioDataset
 
-    SPLIT_FUNCS = {"arctic": split_folder, "citynet": citynet_split}
+    SPLIT_FUNCS = {
+        "arctic": split_folder,
+        "citynet": citynet_split,
+    }
